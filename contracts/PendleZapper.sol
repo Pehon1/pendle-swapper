516712906dc94556cd4ef4b842a969d205b9cdad
Pehon <pehon@ong.ph>
2021-08-13 02:47:10 +0800

    mvp complete

--- contracts/PendleZapper.sol
+++ contracts/PendleZapper.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./ILendingPoolAddressesProvider.sol";
 import "./ILendingPool.sol";
+import "@pendle/core/contracts/interfaces/IPendleRouter.sol";
 
 contract PendleZapper {
 
@@ -20,15 +21,25 @@ contract PendleZapper {
         usdc  0xe22da380ee6B445bb8273C81944ADEB6E8450422
         ausdc 0xe12AFeC5aa12Cf614678f9bFeeB98cA9Bb95b5B0
         https://docs.aave.com/developers/the-core-protocol/lendingpool
+        PendleRouter 0x85dB6E6eDb8EC6DFeC222B80A54Cc7b42F59671e
+        Forge Id is simply byte32 of https://github.com/pendle-finance/pendle-core/blob/38e040b93113a5a8d3814ec814e4188627d92b9a/deployments/kovan.json#L105
 
         Ropsten 
         USDC 0x07865c6e87b9f70255377e024ace6630c1eaa37f 
         ausdc 0x8d939db4c106713600d5e66100ae4156418981a2
         Pool address provider 0x1c8756FD2B28e9426CDBDcC7E3c4d64fa9A54728
+
+        byte32 https://web3-type-converter.onbrn.com/
+
+
+        Kovan contract deployed at 0xD859076DA1FA48393f3a21680A902FEFC39802c6
+
+        Gas cost at 40gwei is 0.0168552eth or $52
      */
 
     IERC20 public usdc = IERC20(address(0xe22da380ee6B445bb8273C81944ADEB6E8450422));
     IERC20 public ausdc = IERC20(address(0xe12AFeC5aa12Cf614678f9bFeeB98cA9Bb95b5B0));
+    IPendleRouter public pendleRouter = IPendleRouter(address(0x85dB6E6eDb8EC6DFeC222B80A54Cc7b42F59671e));
 
     ILendingPoolAddressesProvider public constant addressesProvider = ILendingPoolAddressesProvider(address(0x88757f2f99175387aB4C6a4b3067c77A695b0349));
 
@@ -64,12 +75,25 @@ contract PendleZapper {
     }
 
     function usdcToPendleOTYT(uint256 amount) external {
+        // get aave lending pool address
         ILendingPool lendingPool = ILendingPool(
             addressesProvider.getLendingPool()
         );
+
+        // transfer from user to this contract
         usdc.transferFrom(msg.sender, address(this), amount);
+
+        // approve aave to use the money in this contract
         usdc.approve(address(lendingPool), amount);
+
+        // deposit money into aave and receive aUSDC
         lendingPool.deposit(address(usdc), amount, address(this), 0);
-        ausdc.transfer(msg.sender, amount);
+
+        // approve pendle to take the aUSDC in this contract
+        ausdc.approve(address(pendleRouter), amount);
+
+        bytes32 forgeId = "AaveV2";
+        // tokenise the yield and send to the message sender. 
+        pendleRouter.tokenizeYield(forgeId, address(usdc), 1672272000, amount, msg.sender);
     }
 }
